e446f6e3b704d9faffcb3780c7d84459387cb2c4
aliciasoares <aliciaoliveirasoaress@gmail.com>
2026-03-19 20:32:53 -0300

    Update Calculations.kt

--- app/src/main/java/com/example/calculadoradeimc/datasource/Calculations.kt
+++ app/src/main/java/com/example/calculadoradeimc/datasource/Calculations.kt
@@ -1,6 +1,27 @@
 package com.example.calculadoradeimc.datasource
 
-object Calculations {
+import android.annotation.SuppressLint
 
-    fun calculateIMC(height: String, weight: String, response: (Strin))
+object Calculations {
+    @SuppressLint("Defaultlocale")
+    fun calculateIMC(height: String, weight: String, response: (String, Boolean) -> Unit) {
+        if (height.isNotEmpty() && weight.isNotEmpty()) {
+            val weightFormatted = weight.replace(",", ".").toDoubleOrNull()
+            val heightFormatted = height.toDoubleOrNull()
+            if (weightFormatted != null && heightFormatted != null) {
+                val imc = weightFormatted / (heightFormatted / 100) * (heightFormatted / 100)
+                val imcFormatted = "%.2f".format(imc)
+                when {
+                    imc < 18.5 -> response("IMC: $imcFormatted \n Abaixo do peso", false)
+                    imc in 18.5..24.9 -> response("IMC: $imcFormatted \n Peso normal", false)
+                    imc in 25.0..29.9 -> response("IMC: $imcFormatted \n Sobrepeso", false)
+                    imc in 30.0..34.9 -> response("IMC: $imcFormatted \n Obesidade (Grau 1)", false)
+                    imc in 35.0..39.9 -> response("IMC: $imcFormatted \n Obesidade Severa (Grau 2)", false)
+                    else -> response("IMC: $imcFormatted \n Obesidade Mórbida (Grau 3)", false)
+                }
+            }
+        } else {
+            response("Preencha todos os campos", true)
+        }
+    }
 }
